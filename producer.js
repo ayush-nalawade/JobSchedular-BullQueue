b16ead393b3286ce737d3e098a8803c7f5cadbd2
ayush <ayush.nalawade@slashrtc.com>
2025-05-06 11:00:29 +0530

    remove on complete feature added

--- producer.js
+++ producer.js
@@ -2,4 +2,7 @@
 const myQueue = require('./queue');
 
 // Add a job with some fake data
-myQueue.add({ name: 'Ayush', email: 'ayush@gmail.com' });
+myQueue.add({ name: 'Ayush', email: 'ayush@gmail.com' },{
+  removeOnComplete: true, // remove the job from the queue when it's completed
+  removeOnFail: true, // remove the job from the queue when it fails
+});
